9357fe4d88ca3e403a3facae7a58531d9e68a288
Maxime Verstraete <maxime.verstraete@student.howest.be>
2023-01-08 17:51:27 +0100

    test

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use('/api', blokRouter);
 app.use('/api', showRouter);
 
