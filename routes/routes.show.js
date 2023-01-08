95c3b86914ff129a4c2f3102574702e6fec11b4b
Maxime Verstraete <maxime.verstraete@student.howest.be>
2023-01-09 00:33:16 +0100

    test

--- routes/routes.show.js
+++ routes/routes.show.js
@@ -29,7 +29,7 @@ router.get('/shows', async (req, res) => {
     try {
         // const data = await Show.find();
         // res.json(data);
-        res.json(req.query);
+        res.json(req.query.uitgelicht);
     }
     catch(error) {
         res.status(500).json({message: error.message});
