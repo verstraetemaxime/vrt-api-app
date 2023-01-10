14644f4429c9607cc147dd02f8889b021c90658d
Maxime Verstraete <maxime.verstraete@student.howest.be>
2023-01-10 18:21:14 +0100

    deploy

--- routes/routes.blokken.js
+++ routes/routes.blokken.js
@@ -13,6 +13,7 @@ router.post('/blokken', async (req, res) => {
     try {
         const dataToSave = await data.save();
         res.status(200).json(dataToSave);
+        res.redirect('/blokken');
     }
     catch(error) {
         res.status(400).json({message: error.message})
